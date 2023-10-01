538c143bbbf095a1e1ad9a7f2883684ef47b6a22
inna-tuzhikova <inna-tuzhikova@github.com>
2023-10-01 16:18:17 +0300

    structure

--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,10 @@
 import datetime
-import functools
 import hashlib
-import unittest
 
 from scoring_api.api import constants, handler
 
 
-def cases(test_cases):
-    def decorator(f):
-        @functools.wraps(f)
-        def wrapper(*args):
-            for c in test_cases:
-                new_args = args + (c if isinstance(c, tuple) else (c,))
-                f(*new_args)
-        return wrapper
-    return decorator
-
-
-class TestSuite(unittest.TestCase):
+class TestSuite:
     def setUp(self):
         self.context = {}
         self.headers = {}
@@ -51,81 +38,16 @@ class TestSuite(unittest.TestCase):
         _, code = self.get_response({})
         self.assertEqual(constants.INVALID_REQUEST, code)
 
-    @cases([
-        {
-            'account': 'horns&hoofs',
-            'login': 'h&f',
-            'method': 'online_score',
-            'token': '',
-            'arguments': {}
-        },
-        {
-            'account': 'horns&hoofs',
-            'login': 'h&f',
-            'method': 'online_score',
-            'token': 'sdd',
-            'arguments': {}
-        },
-        {
-            'account': 'horns&hoofs',
-            'login': 'admin',
-            'method': 'online_score',
-            'token': '',
-            'arguments': {}
-        },
-    ])
     def test_bad_auth(self, request):
         _, code = self.get_response(request)
         self.assertEqual(constants.FORBIDDEN, code)
 
-    @cases([
-        {'account': 'horns&hoofs', 'login': 'h&f', 'method': 'online_score'},
-        {'account': 'horns&hoofs', 'login': 'h&f', 'arguments': {}},
-        {'account': 'horns&hoofs', 'method': 'online_score', 'arguments': {}},
-    ])
     def test_invalid_method_request(self, request):
         self.set_valid_auth(request)
         response, code = self.get_response(request)
         self.assertEqual(constants.INVALID_REQUEST, code)
         self.assertTrue(len(response))
 
-    @cases([
-        {},
-        {'phone': '79175002040'},
-        {'phone': '89175002040', 'email': 'stupnikov@otus.ru'},
-        {'phone': '79175002040', 'email': 'stupnikovotus.ru'},
-        {'phone': '79175002040', 'email': 'stupnikov@otus.ru', 'gender': -1},
-        {'phone': '79175002040', 'email': 'stupnikov@otus.ru', 'gender': '1'},
-        {
-            'phone': '79175002040',
-            'email': 'stupnikov@otus.ru',
-            'gender': 1,
-            'birthday': '01.01.1890'
-        },
-        {
-            'phone': '79175002040',
-            'email': 'stupnikov@otus.ru',
-            'gender': 1,
-            'birthday': 'XXX'
-        },
-        {
-            'phone': '79175002040',
-            'email': 'stupnikov@otus.ru',
-            'gender': 1,
-            'birthday': '01.01.2000',
-            'first_name': 1
-        },
-        {
-            'phone': '79175002040',
-            'email': 'stupnikov@otus.ru',
-            'gender': 1,
-            'birthday': '01.01.2000',
-            'first_name': 's',
-            'last_name': 2
-        },
-        {'phone': '79175002040', 'birthday': '01.01.2000', 'first_name': 's'},
-        {'email': 'stupnikov@otus.ru', 'gender': 1, 'last_name': 2},
-    ])
     def test_invalid_score_request(self, arguments):
         request = {
             'account': 'horns&hoofs',
@@ -138,27 +60,6 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(constants.INVALID_REQUEST, code, arguments)
         self.assertTrue(len(response))
 
-    @cases([
-        {'phone': '79175002040', 'email': 'stupnikov@otus.ru'},
-        {'phone': 79175002040, 'email': 'stupnikov@otus.ru'},
-        {
-            'gender': 1,
-            'birthday': '01.01.2000',
-            'first_name': 'a',
-            'last_name': 'b'
-        },
-        {'gender': 0, 'birthday': '01.01.2000'},
-        {'gender': 2, 'birthday': '01.01.2000'},
-        {'first_name': 'a', 'last_name': 'b'},
-        {
-            'phone': '79175002040',
-            'email': 'stupnikov@otus.ru',
-            'gender': 1,
-            'birthday': '01.01.2000',
-            'first_name': 'a',
-            'last_name': 'b'
-        },
-    ])
     def test_ok_score_request(self, arguments):
         request = {
             'account': 'horns&hoofs',
@@ -189,14 +90,6 @@ class TestSuite(unittest.TestCase):
         score = response.get('score')
         self.assertEqual(score, 42)
 
-    @cases([
-        {},
-        {'date': '20.07.2017'},
-        {'client_ids': [], 'date': '20.07.2017'},
-        {'client_ids': {1: 2}, 'date': '20.07.2017'},
-        {'client_ids': ['1', '2'], 'date': '20.07.2017'},
-        {'client_ids': [1, 2], 'date': 'XXX'},
-    ])
     def test_invalid_interests_request(self, arguments):
         request = {
             'account': 'horns&hoofs',
@@ -209,14 +102,6 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(constants.INVALID_REQUEST, code, arguments)
         self.assertTrue(len(response))
 
-    @cases([
-        {
-            'client_ids': [1, 2, 3],
-            'date': datetime.datetime.today().strftime('%d.%m.%Y')
-        },
-        {'client_ids': [1, 2], 'date': '19.07.2017'},
-        {'client_ids': [0]},
-    ])
     def test_ok_interests_request(self, arguments):
         request = {
             'account': 'horns&hoofs',
@@ -238,7 +123,3 @@ class TestSuite(unittest.TestCase):
             self.context.get('nclients'),
             len(arguments['client_ids'])
         )
-
-
-if __name__ == '__main__':
-    unittest.main()
